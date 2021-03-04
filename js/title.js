f92a5904f0237707f8de1619fb4f234f13472da0
Leafy <60797172+iiLeafy@users.noreply.github.com>
2021-03-04 11:51:43 +0200

    yeah add some js

---
+++ js/title.js
@@ -0,0 +1,20 @@
+const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
+let title = '=- stop bark -=';
+let loop = "";
+
+async function start() {
+  while (true) {
+    for (let char of title) {
+      if (loop === title) {
+        loop = "";
+      }
+
+      loop += char;
+      window.document.title = loop;
+      await sleep(750);
+    }
+  }
+}
+(async function() {
+  start()
+})();
